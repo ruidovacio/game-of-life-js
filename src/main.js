1549dcdf7c24dcbf19bc7cc6f56303b6e7171c14
ruidovacio <nikkolazhito@hotmail.com>
2023-04-02 20:38:12 -0300

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-import { checkDirection } from "./checkDirection.js";
-
 //crear la grilla
 const gridSize = 10;
 let isOn = false;
-grid = createGrid(gridSize, false);
+let grid = createGrid(gridSize, false);
 let loop = null;
 
 //accion de botones
@@ -122,4 +120,38 @@ function evolution() {
   }
   grid = JSON.parse(JSON.stringify(newGrid));
   displayGrid();
+}
+
+//chequeo de direcciones
+function checkDirection(y, x, gridSize) {
+  let directions = [
+    { y: y - 1, x: x }, //norte
+    { y: y - 1, x: x + 1 }, //nordeste
+    { y: y, x: x + 1 }, //este
+    { y: y + 1, x: x + 1 }, //sudeste
+    { y: y + 1, x: x }, //sur
+    { y: y + 1, x: x - 1 }, //suroeste
+    { y: y, x: x - 1 }, //oeste
+    { y: y - 1, x: x - 1 }, //noroeste
+  ];
+  for (let i = 0; i < directions.length; i++) {
+    if (
+      directions[i].y < 0 ||
+      directions[i].y > gridSize - 1 ||
+      directions[i].x < 0 ||
+      directions[i].x > gridSize - 1
+    ) {
+      directions[i] = undefined;
+    }
+  }
+  let filteredDirections = directions.filter((el) => el != undefined);
+  let aliveCount = 0;
+  for (let i = 0; i < filteredDirections.length; i++) {
+    let y = filteredDirections[i].y;
+    let x = filteredDirections[i].x;
+    if (grid[y][x].state === true) {
+      aliveCount++;
+    }
+  }
+  return aliveCount;
 }
